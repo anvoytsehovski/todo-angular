4669ee0e458c081edc2ef0f1358d3a8e3b9bfa07
Anderw Voytsehovski <a.voytsehovski@gmail.com>
2020-06-28 17:02:52 +0300

    added subject and subscription objects to display tasks when selecting a category (all tasks have not yet been shown)

--- src/app/services/data-handler.service.ts
+++ src/app/services/data-handler.service.ts
@@ -2,25 +2,30 @@ import { Injectable } from '@angular/core';
 import { Category } from "../model/Category";
 import { TestData } from "../data/TestData";
 import { Task } from "../model/Task";
+import { Subject } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
 
-  constructor() { }
+    tasksSubject = new Subject<Task[]>();
+
+  constructor() {
+
+  }
 
   getCategories(): Category[] {
     return TestData.categories;
   }
 
-  getTasks(): Task[] {
-    return TestData.tasks;
+  fillTasks() {
+      this.tasksSubject.next(TestData.tasks);
   }
 
-  getTaskByCategory(category: Category): Task[] {
+  fillTaskByCategory(category: Category) {
       const tasks = TestData.tasks.filter(task => task.category === category);
-      console.log(tasks);
-      return tasks;
+
+      this.tasksSubject.next(tasks);
   }
 }
