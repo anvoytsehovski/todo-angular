67f57e040cf8a8b9914335d001f60882fe7c9f73
Anderw Voytsehovski <a.voytsehovski@gmail.com>
2020-06-29 19:21:31 +0300

    Replaced by a table with tasks using Angular Material

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataHandlerService } from "../../services/data-handler.service";
 import { Task } from 'src/app/model/Task';
+import { MatTableDataSource } from "@angular/material";
 
 @Component({
   selector: 'app-tasks',
@@ -9,15 +10,36 @@ import { Task } from 'src/app/model/Task';
 })
 export class TasksComponent implements OnInit {
 
+  private displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+  private dataSource: MatTableDataSource<Task>;
+
   tasks: Task[];
 
   constructor(private dataHandler: DataHandlerService) { }
 
   ngOnInit() {
     this.dataHandler.tasksSubject.subscribe(tasks => this.tasks = tasks);
+
+    this.dataSource = new MatTableDataSource();
+
+    this.refreshTable();
   }
 
   toggleTaskCompleted(task: Task) {
     task.completed = !task.completed;
+  }
+
+  private getPriorityColor(task: Task) {
+
+    if (task.priority && task.priority.color) {
+      return task.priority.color;
+    }
+
+    return '#fff';
+  }
+
+  private refreshTable() {
+
+    this.dataSource.data = this.tasks; // обновить источник данных (т.к. данные массива tasks обновились)
   }
 }
